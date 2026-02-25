95a5a13d381677028f490e89fe50b32e522991ec
DAEUN KIM <kimdaeun1230@naver.com>
2026-02-25 13:53:56 +0900

    20260225_리액트스터디

--- section01/chapter11.js
+++ section01/chapter11.js
@@ -4,9 +4,9 @@ function greeting () {
     console.log("안녕하세요!");
 }
 
-//console.log("호출 전");
-//greeting(); //함수 호출할 때 소괄호를 꼭 작성해줘야한다.
-//console.log("호출 후");
+console.log("호출 전");
+greeting(); //함수 호출할 때 소괄호를 꼭 작성해줘야한다.
+console.log("호출 후");
 
 //직사각형 만드는 함수
 function getArea (width, height) {
@@ -19,7 +19,7 @@ getArea(10, 20); //200출력
 getArea(120, 200);
 
 function getArea (width, height) {
-    function another() {
+    function another() {  //함수 안에 함수를 또 만들 수 있음 (중첩 함수)
         console.log("another");
     }
 
@@ -30,3 +30,7 @@ function getArea (width, height) {
 }
 
 let area1 = getArea(10, 20); //area1라는 변수에 200이라는 값이 저장이 됨
+
+//호이스팅
+// -> 끌어올리다 라는 뜻
+// 선언하기 전에 호출해도 문제없이 실행됨
