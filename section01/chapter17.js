f7f3c9de2efcadc73f7f6430d737639c6fa70524
DAEUN KIM <kimdaeun1230@naver.com>
2026-03-04 17:13:50 +0900

    20260304_리액트스터디

--- section01/chapter17.js
+++ section01/chapter17.js
@@ -1,1 +1,26 @@
-// 17강
+// 1. 배열 생성
+let arrA = new Array(); // 배열 생성자
+let arrB = []; //  배열 리터럴 (대부분 사용)
+
+let arrC = [ // 배열 안에는 어떤 형식을 써도 상관없고, 길이도 상관없다
+    1,
+    2,
+    3,
+    true,
+    "hello",
+    null,
+    undefined,
+    () => {},
+    {},
+    [],
+];
+
+// 2. 배열 요소 접근
+let item1 = arrC[0]; // 0 같은 숫자를 인덱스라고 함
+let item2 = arrC[1];
+
+console.log(item1, item2);
+
+// 값을 바꿀 때는 해당하는 값을 다시 정의해주면 됨
+arrC[0] = "hello";
+console.log(arrC); //바뀐 값으로 출력됨
